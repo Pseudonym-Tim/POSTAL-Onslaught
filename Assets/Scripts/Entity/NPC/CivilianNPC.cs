ea7a2540cf5fc7de65c3f5f0895533d609d266f3
Pseudonym-Tim <mute2673@gmail.com>
2024-08-03 06:58:28 -0700

    Added civilian NPC
    They pretty much just run around randomly right now

---
+++ Assets/Scripts/Entity/NPC/CivilianNPC.cs
@@ -0,0 +1,121 @@
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A defenseless scared civilian NPC...
+/// </summary>
+public class CivilianNPC : NPC
+{
+    [SerializeField] private AIState currentAIState = AIState.ROAMING;
+    [SerializeField] private float roamRadius = 5f;
+    [SerializeField] private float roamTimeMin = 0.75f;
+    [SerializeField] private float roamTimeMax = 1.5f;
+    [SerializeField] private float roamSpeedMin = 1.5f;
+    [SerializeField] private float roamSpeedMax = 2.25f;
+    private float roamTimer = 0;
+
+    public override void OnEntitySpawn()
+    {
+        playerEntity = GetPlayer();
+
+        if(LevelNavmesher.IsNavmeshBuilt && NPCNavigation == null)
+        {
+            SetupNPCNavigation(0);
+            BeginRoaming();
+        }
+    }
+
+    public override void OnNavmeshBuilt()
+    {
+        if(NPCNavigation == null)
+        {
+            SetupNPCNavigation(0);
+            BeginRoaming();
+        }
+    }
+
+    protected override void OnEntityUpdate()
+    {
+        switch(currentAIState)
+        {
+            case AIState.ROAMING:
+
+                if(IsReadyForNextRoam)
+                {
+                    NPCNavigation.NavMeshAgentSpeed = Random.Range(roamSpeedMin, roamSpeedMax);
+                    Vector3 destinationPos = LevelNavmesher.GetRandomPosition(EntityPosition, roamRadius);
+                    NPCNavigation.SetDestination(destinationPos);
+                }
+
+                break;
+            case AIState.KNOCKBACK:
+                break;
+            case AIState.DEAD:
+                break;
+        }
+
+        EntityAnim.SetBool("isMoving", NPCNavigation.IsMoving);
+
+        // Flip the sprite based on move direction...
+        if(NPCNavigation.NavMeshAgentVelocity.x != 0)
+        {
+            Vector2 agentVelocity = NPCNavigation.NavMeshAgentVelocity;
+            npcGFX.flipX = agentVelocity.normalized.x < 0;
+        }
+    }
+
+    private void BeginRoaming()
+    {
+        NPCNavigation.StopMoving();
+        roamTimer = Random.Range(roamTimeMin, roamTimeMax);
+        NPCNavigation.NavMeshAgentSpeed = Random.Range(roamSpeedMin, roamSpeedMax);
+        currentAIState = AIState.ROAMING;
+    }
+
+    protected override void OnKnockbackStart()
+    {
+        currentAIState = AIState.KNOCKBACK;
+    }
+
+    protected override void OnKnockbackEnd()
+    {
+        BeginRoaming();
+    }
+
+    protected override void OnDeath()
+    {
+        currentAIState = AIState.DEAD;
+    }
+
+    protected override void OnTakeDamage(DamageInfo damageInfo)
+    {
+        // TODO: Begin running away from player...
+    }
+
+    private bool IsReadyForNextRoam
+    {
+        get
+        {
+            if(roamTimer > 0)
+            {
+                roamTimer = Mathf.Max(roamTimer - Time.deltaTime, 0);
+            }
+            else if(roamTimer <= 0 || NPCNavigation.IsDestinationReached)
+            {
+                roamTimer = Random.Range(roamTimeMin, roamTimeMax);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public enum AIState
+    {
+        ROAMING,
+        KNOCKBACK,
+        DEAD
+    }
+}
